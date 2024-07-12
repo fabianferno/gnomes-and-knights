00e7d4b9605235a7ac54a5dc497aaa6587e427ca
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2024-07-13 04:47:51 +0530

    cleanup

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,21 +1,16 @@
 "use client";
-
-import Button from "@/components/Button";
-import CloseButton from "@/components/CloseButton";
 import Grid from "@/components/Grid";
 import Image from "next/image";
 import WorldCoinConnect from "@/components/WorldCoin";
-// import { ConnectButton } from "@rainbow-me/rainbowkit";
-// import { useAccount } from "wagmi";
 import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
 import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
+import Profile from "@/components/Profile";
 
 export default function Home() {
   const { primaryWallet } = useDynamicContext();
 
-
   return (
-    <main className="mx-5 flex min-h-screen flex-col items-center justify-center pb-10 ">
+    <main className="mx-auto flex flex-col items-center justify-center max-w-sm">
       <div className="relative grid grid-cols-1 container place-items-center">
         <div className="text-center mb-5">
           <div className="text-3xl font-bold">
@@ -27,30 +22,24 @@ export default function Home() {
         </div>
       </div>
 
-      <section className="lg:max-w-6xl lg:w-full">
-        <div className="ring-1 ring-zinc-700 rounded-xl p-1 w-full">
+      <section className="w-sm">
+        <div className="ring-1 ring-zinc-700 rounded-xl p-1 mx-auto w-full">
           {!primaryWallet ? (
             <div className="flex justify-center items-center flex-col">
               <h3 className="text-md mb-5">
                 Connect your wallet to get started
               </h3>
-         <DynamicWidget />
+              <DynamicWidget />
             </div>
           ) : (
             <div className="flex justify-center items-start flex-col ">
               <div className="flex w-full justify-between items-center">
-                   <DynamicWidget />
+                <DynamicWidget />
 
                 <WorldCoinConnect />
               </div>
-
-              <main className="flex min-h-screen flex-col items-center justify-between p-24">
-                <div>
-                  <Button>Play Now</Button>
-                </div>
-                <div className="w-10">
-                  <CloseButton>Close</CloseButton>
-                </div>
+              <main className="flex flex-col items-center justify-center ">
+                <Profile />
                 <div className="w-96 h-96">
                   <Grid
                     grid={[0, 1, 5, 0, 3, 2, 1, 5, 6, 7, 0, 7, 5, 2, 0, 1]}
