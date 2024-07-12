c20794b532ae9511ca969e8f8d4c43b765d72b78
Fabian Ferno <fabianferno@gmail.com>
2024-07-13 02:12:58 +0530

    Add rainbow kit providers

---
+++ web/src/components/Providers.tsx
@@ -0,0 +1,68 @@
+"use client";
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
+import { sepolia, baseSepolia } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { type Chain } from "viem";
+
+export const mantleSepolia = {
+  id: 5003,
+  name: "Mantle Sepolia",
+  nativeCurrency: { name: "MNT", symbol: "MNT", decimals: 18 },
+  rpcUrls: {
+    default: { http: ["https://rpc.sepolia.mantle.xyz"] },
+  },
+  blockExplorers: {
+    default: {
+      name: "Mantle Explorer",
+      url: "https://explorer.sepolia.mantle.xyz",
+    },
+  },
+} as const satisfies Chain;
+
+const queryClient = new QueryClient();
+
+const config = getDefaultConfig({
+  appName: "Connekt",
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID || "",
+  chains: [sepolia, mantleSepolia],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+          theme={darkTheme({
+            accentColor: "#111111",
+            accentColorForeground: "white",
+            borderRadius: "medium",
+            fontStack: "system",
+            overlayBlur: "small",
+          })}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
