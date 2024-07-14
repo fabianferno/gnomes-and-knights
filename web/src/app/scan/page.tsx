386bade176062f21a22bb22f66a05d7e5b70ec03
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-07-14 03:50:17 +0200

    added PlayerStats

--- web/src/app/scan/page.tsx
+++ web/src/app/scan/page.tsx
@@ -8,6 +8,7 @@ import { mintApe } from "@/lib/ContractHelpers/mint";
 import { duel } from "@/lib/ContractHelpers/duel";
 import { publicClient } from "@/lib/Client";
 import { updateMatrix } from "@/lib/ContractHelpers/updateMatrix";
+import { viewStats } from "@/lib/ContractHelpers/viewStats";
 
 export default function Home() {
   return (
@@ -20,11 +21,9 @@ export default function Home() {
         // const hash = await approveApe("04:3d:5e:22:4f:13:90");
         // console.log(hash);
         // const hash = await mintApe("04:12:68:22:4f:13:90");
-
         // console.log(hash);
         // const hash = await createProfile("04:12:68:22:4f:13:90");
         // console.log(hash);
-
         // const hash = await duel(
         //   "04:32:7c:9a:51:13:90",
         //   "0xeB20282EB31bc083EBd00645cA4c5d323fDf7fB8"
@@ -33,14 +32,17 @@ export default function Home() {
         //   hash: hash,
         // });
         // console.log(hash1);
-
-        const hash1 = await updateMatrix("04:12:68:22:4f:13:90", [
-          [20, 76, 30, 10],
-          [23, 54, 34, 21],
-          [22, 56, 34, 53],
-          [23, 54, 34, 21],
-        ]);
-        console.log(hash1);
+        // const hash1 = await updateMatrix("04:12:68:22:4f:13:90", [
+        //   [20, 76, 30, 10],
+        //   [23, 54, 34, 21],
+        //   [22, 56, 34, 53],
+        //   [23, 54, 34, 21],
+        // ]);
+        // console.log(hash1);
+        // const hash = await viewStats(
+        //   "0xeB20282EB31bc083EBd00645cA4c5d323fDf7fB8"
+        // );
+        // console.log(hash);
       }}
     >
       getbalance
---
+++ web/src/lib/ContractHelpers/viewStats.ts
@@ -0,0 +1,15 @@
+import { keccak256, toUtf8Bytes } from "ethers";
+import { publicClient, walletClient } from "../Client";
+import { abi } from "../abi/gnomesandknights";
+import { privateKeyToAccount } from "viem/accounts";
+import { apecoincontract, gnomeandknightscontract } from "../const";
+
+export const playerStats = async (address: string) => {
+  const hash = await publicClient.readContract({
+    address: gnomeandknightscontract,
+    abi: abi,
+    functionName: "players",
+    args: [address],
+  });
+  return hash;
+};
